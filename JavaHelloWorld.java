01ca04a753ac0600279a839aaaca4bea9ae55470
vemular1 <vemula.raju88@gmail.com>
2016-10-31 19:59:47 +0000

    changes

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,6 @@
 public class JavaHelloWorld
 {	
-	publc static void main(String [] args)
+	public static void main(String [] args)
 	{
 		System.out.println("Hello World");
 		System.out.println("Hello again");
